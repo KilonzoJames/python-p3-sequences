63a5c9413003458cd07576e6aced615f35c49b18
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-17 18:56:21 +0300

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
+# def return_fibonacci(length):
+#     if length == 0:
+#         fibonacci_list = []
+#     elif length == 1:
+#         fibonacci_list = [0]
+#     else:
+#         fibonacci_list = [0, 1]
+#     for _ in range(2,length):
+#         fibonacci_list.append(fibonacci_list[-1]+fibonacci_list[-2])
+#     print(fibonacci_list)
+# print(return_fibonacci(9))
+
+
+
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [] if length == 0 else [0] if length == 1 else [0, 1]
+
+    for _ in range(2, length):
+        next_number = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_number)
+
+    print(fibonacci_list)
+
+print(print_fibonacci(9))
